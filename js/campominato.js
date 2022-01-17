a14cd3bc3766a395b8d8da138747eb5ae0d44b6b
illupovero <davideluporini1996@gmail.com>
2022-01-17 16:19:53 +0100

    added  randoms numbers

--- js/campominato.js
+++ js/campominato.js
@@ -25,31 +25,46 @@ console.log('js ok')
 // cosa fare sotto banco? in base alla difficoltà eseguire un sistema di generazione celle o un'altro
 
 
-
 // celle:
 // funzione che mi generi le celle
 // creare tramite la funzione i div che dovranno avere classe per colorarsi di blu
+// FINE COMMENTI
 
 
-const selectDifficultyEasy = document.getElementById('easy')
-const selectDifficultyNormal = document.getElementById('normal')
-const selectDifficultyHard = document.getElementById('hard')
-const startGame = document.getElementById('play')
-
-let cell = 8;
-let columns = 8;
-
-
+// funzioni
 // calcolo un numero casuale 
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+
+// VARIABILI
+const selectDifficultyEasy = document.getElementById('easy');
+const selectDifficultyNormal = document.getElementById('normal');
+const selectDifficultyHard = document.getElementById('hard');
+const startGame = document.getElementById('play');
+
+let cells = 8;
+let columns = 8;
+let totalCells = cells * columns;
+
+console.log(cells)
+console.log(columns)
+console.log(totalCells)
+
+
 
 
 // dichiaro numeri per difficoltà
-const numberEasy = getRandomNumber(1, 100)
-const numberNormal = getRandomNumber(1, 81)
-const numberHard = getRandomNumber(1, 49)
+const numberEasy = getRandomNumber(1, 100);
+const numberNormal = getRandomNumber(1, 81);
+const numberHard = getRandomNumber(1, 49);
 
 
-console.log(numberEasy)
-console.log(numberNormal)
-console.log(numberHard)
+console.log(numberEasy);
+console.log(numberNormal);
+console.log(numberHard);
+
+
+// evento sul click in easy
+selectDifficultyEasy.addEventListener('click', function() {
+
+})
